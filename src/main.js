bf7b5c1f434e1ea1f4405ad358ccdc0863dc6933
Carlos Treminio <ctreminiom079@gmail.com>
2019-06-09 19:14:20 -0600

    Changed the UI framework

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-import iView from 'iview';
-import 'iview/dist/styles/iview.css';
-
-Vue.use(iView);
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
