8f24f814b318780453dae50c57365085e7d9333e
aleazofeifam <aleazofeifam@gmail.com>
2019-08-07 17:55:03 -0600

    Update clients and rooms with filter in tables

--- src/views/admin/queries/Rooms.vue
+++ src/views/admin/queries/Rooms.vue
@@ -16,7 +16,55 @@
 
       <a-col :span="24">
         <a-card title="Roles">
-          <a-table :columns="columns" :dataSource="dataSource"></a-table>
+          <a-table :columns="columns" :dataSource="dataSource">
+            <div
+              slot="filterDropdown"
+              slot-scope="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }"
+              class="custom-filter-dropdown"
+            >
+              <a-input
+                v-ant-ref="c => searchInput = c"
+                :placeholder="`Search ${column.dataIndex}`"
+                :value="selectedKeys[0]"
+                @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
+                @pressEnter="() => handleSearch(selectedKeys, confirm)"
+                style="width: 188px; margin-bottom: 8px; display: block;"
+              />
+              <a-button
+                type="primary"
+                @click="() => handleSearch(selectedKeys, confirm)"
+                icon="search"
+                size="small"
+                style="width: 90px; margin-right: 8px"
+              >Search</a-button>
+              <a-button
+                @click="() => handleReset(clearFilters)"
+                size="small"
+                style="width: 90px"
+              >Reset</a-button>
+            </div>
+            <a-icon
+              slot="filterIcon"
+              slot-scope="filtered"
+              type="search"
+              :style="{ color: filtered ? '#108ee9' : undefined }"
+            />
+            <template slot="customRender" slot-scope="text">
+              <span v-if="searchText">
+                <template
+                  v-for="(fragment, i) in text.toString().split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))"
+                >
+                  <mark
+                    v-if="fragment.toLowerCase() === searchText.toLowerCase()"
+                    :key="i"
+                    class="highlight"
+                  >{{fragment}}</mark>
+                  <template v-else>{{fragment}}</template>
+                </template>
+              </span>
+              <template v-else>{{text}}</template>
+            </template>
+          </a-table>
         </a-card>
       </a-col>
     </a-row>
@@ -32,16 +80,46 @@ export default {
   data() {
     return {
       dataSource: null,
+      searchText: "",
+      searchInput: null,
       columns: [
         {
-          title: "Room Number", 
+          title: "Room Number",
           dataIndex: "roomnumber", //Check the name
-          key: "roomnumber" //Check the name
+          key: "roomnumber", //Check the name
+          scopedSlots: {
+            filterDropdown: "filterDropdown",
+            filterIcon: "filterIcon",
+            customRender: "customRender"
+          },
+          onFilter: (value, record) =>
+            record.name.toLowerCase().includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              }, 0);
+            }
+          }
         },
         {
           title: "Status",
-          dataIndex: "status",//Check the name
-          key: "status"//Check the name
+          dataIndex: "status", //Check the name
+          key: "status", //Check the name
+          scopedSlots: {
+            filterDropdown: "filterDropdown",
+            filterIcon: "filterIcon",
+            customRender: "customRender"
+          },
+          onFilter: (value, record) =>
+            record.name.toLowerCase().includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              }, 0);
+            }
+          }
         }
       ]
     };
@@ -57,6 +135,16 @@ export default {
         }
       );
     },
+
+    handleSearch(selectedKeys, confirm) {
+      confirm();
+      this.searchText = selectedKeys[0];
+    },
+
+    handleReset(clearFilters) {
+      clearFilters();
+      this.searchText = "";
+    }
   }
 };
 </script>
